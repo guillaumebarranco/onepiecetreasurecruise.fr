d78e17832b765859c2a90dad451de18818c76763
guillaumebarranco <guillaume.barranco8@hotmail.fr>
2021-06-07 18:40:15 +0200

    Update Queen tm guide

--- shared/button-toggle.js
+++ shared/button-toggle.js
@@ -14,6 +14,22 @@ function initButtons() {
       }
     });
   });
+
+  document.querySelectorAll(".tm-intro button").forEach((button) => {
+    button.addEventListener("click", (element) => {
+
+      const target =
+        element.target.attributes["data-thetarget"].textContent;
+        console.log('target', target)
+      const targetDisplay = document.querySelector(target).style.display;
+
+      if (!targetDisplay || targetDisplay === "none") {
+        document.querySelector(target).style.display = "block";
+      } else {
+        document.querySelector(target).style.display = "none";
+      }
+    });
+  });
 }
 
 setTimeout(() => {
