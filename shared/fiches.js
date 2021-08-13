a7894ef4a753c8be899ab19258b60a6d5f551232
Terbis <manoel.hallu@yahoo.fr>
2021-08-13 02:25:30 +0200

    completed monet and baby 5 event (rewards, mission guide), completed caesar clown, arena guide, changed WA, BOMB+, POISON orbs colors

--- shared/fiches.js
+++ shared/fiches.js
@@ -1,64 +1,74 @@
-function replaceOrbsColors(chosenSection = '.fiche-table') {
-
-  $(chosenSection).each((index) => {
-  });
+function replaceOrbsColors(chosenSection = ".fiche-table") {
+  $(chosenSection).each((index) => {});
 
   let $section = $(chosenSection).html();
 
   const orbs = [
     {
-      name: 'STR',
+      name: "STR",
       color: $STR,
     },
     {
-      name: 'DEX',
+      name: "DEX",
       color: $DEX,
     },
     {
-      name: 'QCK',
+      name: "QCK",
       color: $QCK,
     },
     {
-      name: 'PSY',
+      name: "PSY",
       color: $PSY,
     },
     {
-      name: 'INT',
+      name: "INT",
       color: $INT,
     },
     {
-      name: 'BLK',
-      color: 'darkslateblue',
+      name: "BLK",
+      color: "darkslateblue",
     },
     {
-      name: 'TND',
-      color: 'peru',
+      name: "TND",
+      color: "peru",
     },
     {
-      name: 'RCV',
-      color: '#BC5C0C',
+      name: "RCV",
+      color: "#BC5C0C",
     },
     {
-      name: 'BOMB',
-      color: 'maroon',
+      name: "SEMLA",
+      color: "saddlebrown",
     },
     {
-      name: 'EMP',
-      color: '#777',
+      name: "BOMB",
+      color: "maroon",
     },
     {
-      name: 'WA',
-      color: 'saddlebrown',
+      name: "BOMB+",
+      color: "#FF6500",
     },
     {
-      name: 'G',
-      color: 'orange',
-    }
+      name: "EMP",
+      color: "#777",
+    },
+    {
+      name: "WA",
+      color: "#B10000",
+    },
+    {
+      name: "G",
+      color: "orange",
+    },
+    {
+      name: "POISON",
+      color: "#A83CCC",
+    },
   ];
 
   if ($section) {
-    orbs.forEach(orb => {
-      const regex = new RegExp(escapeRegExp(`[${orb.name}]`), 'gi');
+    orbs.forEach((orb) => {
+      const regex = new RegExp(escapeRegExp(`[${orb.name}]`), "gi");
 
       $section = $section.replace(
         regex,
@@ -70,117 +80,118 @@ function replaceOrbsColors(chosenSection = '.fiche-table') {
   }
 }
 
-function replaceTypesColors(chosenSection = '.fiche-table') {
+function replaceTypesColors(chosenSection = ".fiche-table") {
   let $section = $(chosenSection).html();
 
   const types = [
     {
-      name: 'STR',
+      name: "STR",
       color: $STR,
     },
     {
-      name: 'DEX',
+      name: "DEX",
       color: $DEX,
     },
     {
-      name: 'QCK',
+      name: "QCK",
       color: $QCK,
     },
     {
-      name: 'PSY',
+      name: "PSY",
       color: $PSY,
     },
     {
-      name: 'INT',
+      name: "INT",
       color: $INT,
     },
   ];
 
   if ($section) {
-    types.forEach(type => {
-      const regex1 = new RegExp(escapeRegExp(`${type.name} `), 'gi');
+    types.forEach((type) => {
+      const regex1 = new RegExp(escapeRegExp(`${type.name} `), "gi");
 
       $section = $section.replace(
         regex1,
         `<strong style="color: ${type.color};">${type.name} </strong>`
       );
 
-      const regex2 = new RegExp(escapeRegExp(` ${type.name}.`), 'gi');
+      const regex2 = new RegExp(escapeRegExp(` ${type.name}.`), "gi");
 
       $section = $section.replace(
         regex2,
         `<strong style="color: ${type.color};"> ${type.name}.</strong>`
       );
 
-      const regex3 = new RegExp(escapeRegExp(` ${type.name}<`), 'gi');
+      const regex3 = new RegExp(escapeRegExp(` ${type.name}<`), "gi");
 
       $section = $section.replace(
         regex3,
         `<strong style="color: ${type.color};"> ${type.name}</strong><`
       );
 
-      const regex5 = new RegExp(escapeRegExp(` ${type.name},`), 'gi');
+      const regex5 = new RegExp(escapeRegExp(` ${type.name},`), "gi");
 
       $section = $section.replace(
         regex5,
         `<strong style="color: ${type.color};"> ${type.name}</strong>,`
       );
 
-      const regex4 = new RegExp(escapeRegExp(`<strong>${type.name}</strong>`), 'gi');
+      const regex4 = new RegExp(
+        escapeRegExp(`<strong>${type.name}</strong>`),
+        "gi"
+      );
 
       $section = $section.replace(
         regex4,
         `<strong style="color: ${type.color};">${type.name}</strong>`
       );
-
-
     });
 
     $(chosenSection).html($section);
   }
 }
 
-function addIconsToClasses(chosenSection = '.fiche-table') {
+function addIconsToClasses(chosenSection = ".fiche-table") {
   const classes = [
     {
-      name: 'Ravageur',
-      icon: 'https://i.imgur.com/97ymskP.png',
+      name: "Ravageur",
+      icon: "https://i.imgur.com/97ymskP.png",
     },
     {
-      name: 'Tenace',
-      icon: 'https://i.imgur.com/EA0ZMlX.png',
+      name: "Tenace",
+      icon: "https://i.imgur.com/EA0ZMlX.png",
     },
     {
-      name: 'Cogneur',
-      icon: 'https://i.imgur.com/Bb886qJ.png',
+      name: "Cogneur",
+      icon: "https://i.imgur.com/Bb886qJ.png",
     },
     {
-      name: 'Intellectuel',
-      icon: 'https://i.imgur.com/LsINwdq.png',
+      name: "Intellectuel",
+      icon: "https://i.imgur.com/LsINwdq.png",
     },
     {
-      name: 'Libre',
-      icon: 'https://i.imgur.com/zVl1bkZ.png',
+      name: "Libre",
+      icon: "https://i.imgur.com/zVl1bkZ.png",
     },
     {
-      name: 'Tireur',
-      icon: 'https://i.imgur.com/EgrFZDY.png',
+      name: "Tireur",
+      icon: "https://i.imgur.com/EgrFZDY.png",
     },
     {
-      name: 'Sabreur',
-      icon: 'https://i.imgur.com/NPdyphi.png',
+      name: "Sabreur",
+      icon: "https://i.imgur.com/NPdyphi.png",
     },
     {
-      name: 'Ambitieux',
-      icon: 'https://i.imgur.com/KD5mQrh.png',
+      name: "Ambitieux",
+      icon: "https://i.imgur.com/KD5mQrh.png",
     },
   ];
 
   $(`${chosenSection} .character-classes`).each((index) => {
     let $section = $($(`${chosenSection} .character-classes`)[index]).html();
 
-    classes.forEach(characterClass => {
-      const regex1 = new RegExp(escapeRegExp(`${characterClass.name}`), 'gi');
+    classes.forEach((characterClass) => {
+      const regex1 = new RegExp(escapeRegExp(`${characterClass.name}`), "gi");
 
       $section = $section.replace(
         regex1,
@@ -193,17 +204,17 @@ function addIconsToClasses(chosenSection = '.fiche-table') {
 }
 
 function escapeRegExp(text) {
-  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 }
 
-jQuery(function(){
+jQuery(function () {
   $(function () {
-      $(window).scroll(function () {
-          if ($(this).scrollTop() > 200 ) {
-              $('#scrollUp').css('right');
-          } else {
-              $('#scrollUp').removeAttr( 'style' );
-          }
-      });
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        $("#scrollUp").css("right");
+      } else {
+        $("#scrollUp").removeAttr("style");
+      }
+    });
   });
 });
